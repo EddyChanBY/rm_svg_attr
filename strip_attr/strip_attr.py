37bec25e0dedafbb7d0722bde33f95b8104b43a9
Chan Bang Yuan <chanbangyuan@yahoo.com>
2020-05-09 11:34:52 +0800

    Force convert to utf8 and enhance xml tag writing

--- strip_attr/strip_attr.py
+++ strip_attr/strip_attr.py
@@ -1,7 +1,34 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
-# Get file names input
 import sys, getopt
+
+
+def ensure_UTF8(value):
+    try:
+        return str(value, errors="ignore", encoding="utf-8")
+    except TypeError:  # Wasn't a bytes object, no need to decode
+        return str(value)
+
+
+def insert_xml_tag(input_str):
+    # Force utf-8 encoded
+    input_content = ensure_UTF8(input_str)
+
+    if input_content.find("<?xml") == -1:
+        # no xml at all, add UFT-8 tag
+        input_content = '<?xml version="1.0" encoding="UTF-8"?>' + input_content
+        xml_status = "added"
+    elif input_content.find('encoding="UTF-8"') != -1:
+        xml_status = "is there already"
+    else:
+        # xml tag found but no UTF-8 attribute, add it back
+        mark_pos = input_content.find("?>")
+        input_content = (
+            input_content[:mark_pos] + ' encoding="UTF-8"' + input_content[mark_pos:]
+        )
+        xml_status = "is inserted into existing xml tag"
+
+    return input_content, xml_status
 
 
 def generate_sequential_file(file_name):
@@ -46,14 +73,9 @@ def strip_attr(attr_to_strip, input_content):
         # establish exit condition when the substr cannot be found.
         if input_content.find(attr_to_strip, from_pos) == -1:
             # check before adding the xml tag before writing to file
-            if input_content.find("<?xml") == -1:
-                # no xml at all, add UFT-8 tag
-                input_content = '<?xml version="1.0" encoding="UTF-8"?>' + input_content
-                xml_status = "added"
-            elif input_content.find('<?xml version="1.0" encoding="UTF-8"?>') != -1:
-                xml_status = "is there already"
-            else:
-                xml_status = "is NOT added because unknown value of xml tag exist"
+            xml_done = insert_xml_tag(input_content)
+            input_content = xml_done[0]
+            xml_status = xml_done[1]
             break
         from_pos = input_content.find(attr_to_strip, from_pos)
         # check if the character infront of from_pos is a space
