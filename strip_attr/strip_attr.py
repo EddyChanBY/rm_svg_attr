defb82edb3ba2ac944ef260130bd18605e32bb5f
Chan Bang Yuan <chanbangyuan@yahoo.com>
2020-05-09 09:02:40 +0800

    detact delimiters and space

--- strip_attr/strip_attr.py
+++ strip_attr/strip_attr.py
@@ -22,13 +22,11 @@ def generate_sequential_file(file_name):
     ):
         # net_file_str is in sequencal number, increment by 1
         current_squence = int(net_file_str[len(net_file_str) - 2 :])
-        if current_squence >= 9:
+        if current_squence < 9:
             new_sub_str = "-0" + str(current_squence + 1)
         else:
             new_sub_str = "-" + str(current_squence + 1)
-            new_file_name = (
-                net_file_str[0 : len(net_file_str) - 3] + new_sub_str + ext_str
-            )
+        new_file_name = net_file_str[0 : len(net_file_str) - 3] + new_sub_str + ext_str
     else:
         new_file_name = net_file_str[0 : len(net_file_str)] + "-01" + ext_str
     return new_file_name
@@ -40,24 +38,55 @@ def strip_attr(attr_to_strip, input_content):
     to_pos = 0
     new_str = ""
     strip_counter = 0
+    xml_status = ""
+    quotation_mark = ""
+    illegit_count = 0
 
     while True:
         # establish exit condition when the substr cannot be found.
-        if input_content.find(attr_to_strip) == -1:
-            # add the xml tag before writing to file
-            input_content = '<?xml version="1.0" encoding="UTF-8"?>' + input_content
+        if input_content.find(attr_to_strip, from_pos) == -1:
+            # check before adding the xml tag before writing to file
+            if input_content.find("<?xml") == -1:
+                # no xml at all, add UFT-8 tag
+                input_content = '<?xml version="1.0" encoding="UTF-8"?>' + input_content
+                xml_status = "added"
+            elif input_content.find('<?xml version="1.0" encoding="UTF-8"?>') != -1:
+                xml_status = "is there already"
+            else:
+                xml_status = "is NOT added because unknown value of xml tag exist"
             break
         from_pos = input_content.find(attr_to_strip, from_pos)
-        # from_pos + 1 to capture the space character infront.
-        to_pos = input_content.find('"', from_pos + 1)
-        # to_pos + 1 to include the last character.
-        to_pos = input_content.find('"', to_pos + 1)
-        # in python 3, replace alway give a new copy of the str.
-        # # need a new str to capture the replaced str.
-        new_str = input_content.replace(input_content[from_pos - 1 : to_pos + 1], "")
-        strip_counter = strip_counter + 1
-        input_content = new_str
-    return strip_counter, input_content
+        # check if the character infront of from_pos is a space
+        if input_content[from_pos - 1] == " ":
+            # from_pos - 1 to capture the space character infront.
+            from_pos = from_pos - 1
+        # check single or double quotation marks are used
+        if (
+            input_content.find('"', from_pos + 1, from_pos + len(attr_to_strip) + 6)
+            != -1
+        ):
+            # it is double quotation mark
+            quotation_mark = '"'
+        elif (
+            input_content.find("'", from_pos + 1, from_pos + len(attr_to_strip) + 6)
+            != -1
+        ):
+            quotation_mark = "'"
+        else:
+            illegit_count = illegit_count + 1
+            from_pos = from_pos + len(attr_to_strip) + 1
+            quotation_mark = ""
+        # do the stripping only when legit
+        if quotation_mark != "":
+            to_pos = input_content.find(quotation_mark, from_pos + 1)
+            # to_pos + 1 to include the last character.
+            to_pos = input_content.find(quotation_mark, to_pos + 1)
+            # in python 3, replace alway give a new copy of the str.
+            # # need a new str to capture the replaced str.
+            new_str = input_content.replace(input_content[from_pos : to_pos + 1], "")
+            strip_counter = strip_counter + 1
+            input_content = new_str
+    return strip_counter, input_content, xml_status, illegit_count
 
 
 def open_text_file(filename):
@@ -152,8 +181,15 @@ def main(argv):
     # write file
     write_text_file(outputfile, svg_content)
     print(final_content[0], " instances of <", strip_str, "> stripped.")
-    print("and one xml utf-8 tag added.")
-    print("Results outputed to file ", outputfile)
+    if final_content[3] != 0:
+        print(
+            final_content[3],
+            " instances of <",
+            strip_str,
+            "> NOT stripped because they don't have legit delimiters.",
+        )
+    print("xml utf-8 tag", final_content[2])
+    print("Results outputed to file", outputfile)
 
 
 if __name__ == "__main__":
